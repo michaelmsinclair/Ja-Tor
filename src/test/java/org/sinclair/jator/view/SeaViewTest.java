322a7c0ae1a4f054af8c05ba2ccaa1e398b8425d
Michael Sinclair <michaelmsinclair@gmail.com>
2017-12-28 13:55:52 -0500

    No test, yet, for display
    As the simulation is random, it is difficult to predict without having
    run it. Running it to produce results to compare is circular and not
    good practice.

---
+++ src/test/java/org/sinclair/jator/view/SeaViewTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sinclair.jator.view;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author michael
+ */
+public class SeaViewTest {
+    
+    public SeaViewTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of display method, of class SeaView.
+     */
+    @Test
+    public void testDisplay() {
+        System.out.println("display");
+//        SeaView instance = null;
+//        instance.display();
+        // display() is complex, no test yet.
+//        fail("The test case is a prototype.");
+    }
+    
+}
