0fe00b771f5c97f9d3ef29eb9c07d4a6df3e6968
Michael Sinclair <michaelmsinclair@gmail.com>
2017-12-29 21:38:25 -0500

    Adding color and shape to class Fish
    Can't be arsed to set these a better way than in the constructor. A
    builder pattern seems overkill, but maybe that's my naive understanding
    of how to build things.

--- src/main/java/org/sinclair/jator/model/Fish.java
+++ src/main/java/org/sinclair/jator/model/Fish.java
@@ -12,11 +12,12 @@ import java.util.Random;
  * @author michael
  */
 public class Fish extends SeaCreature {
+    
+    static int color = 0xff00;
+    static String shape = "f";
 
     public Fish(Sea sea, SeaPosition pos, int spawnAge, int starveAge, Random random, int parent) {
-        super(sea, pos, spawnAge, starveAge, random, parent);
-        this.color = 0xff00;
-        this.shape = "f";
+        super(sea, pos, spawnAge, starveAge, random, parent, Fish.color, Fish.shape);
     }
 
     @Override
